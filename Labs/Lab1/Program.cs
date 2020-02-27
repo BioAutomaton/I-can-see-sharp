86922165d113d43e2a7e41f2fe7ef29408fd8d48
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-02-27 17:08:00 +0200

    Minor changes

--- Labs/Lab1/Program.cs
+++ Labs/Lab1/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*Вариант 22. Реализовать функцию возведения в куб частного двух целых чисел.*/
+
+using System;
 
 namespace Lab1
 {
@@ -10,8 +8,13 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            /*Variables init*/
+
             bool correct = false;
             Int32 first = 0, second = 0;
+
+            /*Entering of the first number*/
+
             do
             {
                 Console.WriteLine("Enter first value. Must be an integer.");
@@ -19,12 +22,16 @@ namespace Lab1
                 Console.WriteLine(correct ? "\n" : "Error. Try again.");
             } while (!correct);
 
+            /*Entering of the second number*/
+
             do
             {
                 Console.WriteLine("Enter second value. Must be an integer.");
                 correct = Int32.TryParse(Console.ReadLine(), out second);
                 Console.WriteLine(correct ? "\n" : "Error. Try again.");
             } while (!correct);
+
+            /*Calculatiuon & output*/
 
             float result = (float)first / (float)second;
             Console.WriteLine("Square of the private of first and second value is:\t({0}/{1})^3 = {2}", first, second, Math.Pow(result, 3));
