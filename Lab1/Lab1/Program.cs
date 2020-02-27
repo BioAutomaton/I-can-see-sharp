b73527cc09580d915916a27ab2fb4d58dfd1dd8e
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-02-27 15:29:59 +0200

    New solution has been added. Now it contains code for Lab1 (have been copied from BabySteps sol).

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool correct = false;
+            Int32 first = 0, second = 0;
+            do
+            {
+                Console.WriteLine("Enter first value. Must be an integer.");
+                correct = Int32.TryParse(Console.ReadLine(), out first);
+                Console.WriteLine(correct ? "\n" : "Error. Try again.");
+            } while (!correct);
+
+            do
+            {
+                Console.WriteLine("Enter second value. Must be an integer.");
+                correct = Int32.TryParse(Console.ReadLine(), out second);
+                Console.WriteLine(correct ? "\n" : "Error. Try again.");
+            } while (!correct);
+
+            float result = (float)first / (float)second;
+            Console.WriteLine("Square of the private of first and second value is:\t({0}/{1})^3 = {2}", first, second, Math.Pow(result, 3));
+
+            Console.ReadLine();
+        }
+    }
+}
