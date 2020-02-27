336d7208f095df482e276a1be2196aca6b2c909e
Михаил Потёмкин <michael.potyomkin@gmail.com>
2020-02-27 09:06:45 +0200

    Edited so that now the programm will calculate fractional part.

--- BabySteps/BabySteps/Program.cs
+++ BabySteps/BabySteps/Program.cs
@@ -10,21 +10,20 @@ namespace BabySteps
             Int32 first = 0, second = 0;
             do
             {
-                Console.WriteLine("Enter first value. Mest be an integer.");
+                Console.WriteLine("Enter first value. Must be an integer.");
                 correct = Int32.TryParse(Console.ReadLine(), out first);
-                Console.WriteLine(correct == true ? "Ok" : "Error. Try again.");
+                Console.WriteLine(correct ? "\n" : "Error. Try again.");
             } while (!correct);
-            Console.WriteLine();
 
             do
             {
-                Console.WriteLine("Enter second value. Mest be an integer.");
+                Console.WriteLine("Enter second value. Must be an integer.");
                 correct = Int32.TryParse(Console.ReadLine(), out second);
-                Console.WriteLine(correct == true ? "Ok" : "Error. Try again.");
+                Console.WriteLine(correct ? "\n" : "Error. Try again.");
             } while (!correct);
 
-            int result = first / second;
-            Console.WriteLine("\n" + Math.Pow(result, 3));
+            float result = (float)first / (float)second;
+            Console.WriteLine("Square of the private of first and second value is:\t({0}/{1})^3 = {2}", first, second, Math.Pow(result, 3));
 
             Console.ReadLine();
         }
