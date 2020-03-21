06fb3953bc99b4356adbeffb8ddc1bd5ad6ee877
Michael Potyomkin <54860902+BioAutomaton@users.noreply.github.com>
2020-03-21 14:47:25 +0200

    Lab2 done

--- Labs/Lab2/Lab2/Program.cs
+++ Labs/Lab2/Lab2/Program.cs
@@ -10,10 +10,41 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
-            quadrangle test = new quadrangle();
-            test.print();
+            int N = 0;
+            bool isCorrect;
+            do
+            {
+                isCorrect = Int32.TryParse(Console.ReadLine(), out N);
+                if (!isCorrect) { Console.WriteLine("Wrong entering."); }
+            } while (!isCorrect);
+
+            quadrangle[] array = new quadrangle[N];
+            double avgSquare = 0;
+            int lesserSuqareCounter = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = new quadrangle();
+                array[i].PrintPoints();
+                array[i].SideLengt();
+                Console.WriteLine("Perimeter = " + array[i].Perimeter());
+                Console.WriteLine("Suare = " + array[i].Square());
+                array[i].Diagonals();
+
+                avgSquare += array[i].square;
+                Console.WriteLine("-----------------------------------");
+            }
+            avgSquare /= N;
+            Console.WriteLine("Average suare of quadrangles: " + avgSquare);
+            for (int i = 0; i < N; i++)
+            {
+                if (array[i].square < avgSquare) { lesserSuqareCounter++; }
+            }
+            Console.WriteLine("Quadrangles with suare below average: " + lesserSuqareCounter);
             Console.ReadLine();
         }
-        
+
     }
 }
+
+
--- Labs/Lab2/Lab2/quadrangle.cs
+++ Labs/Lab2/Lab2/quadrangle.cs
@@ -11,31 +11,71 @@ namespace Lab2
         public int x;
         public int y;
     }
-        class quadrangle
+
+    public class quadrangle
     {
+        int n = 4;
         point2D[] points;
+        double[] length;
+        double[] diagonal;
+        double perimeter = 0;
+        public double square = 0;
         Random r = new Random();
 
         public quadrangle()
-        {
-            this.points = new point2D[4];
-
+        {         
+            points = new point2D[4];
             for (int i = 0; i < points.Length; i++)
             {
-                points[i].x = r.Next(-10,10);
-                points[i].y = r.Next(-10,10);
-   
+                points[i].x = r.Next(-9, 9);
+                points[i].y = r.Next(-9, 9);
             }
         }
 
-        public void print()
+        public void PrintPoints()
         {
             for (int i = 0; i < points.Length; i++)
             {
-                Console.WriteLine($"X = {points[i].x} Y = {points[i].y}");
+                Console.WriteLine($"X = {points[i].x}\t Y = {points[i].y}");
+                
             }
         }
-        public void sideLengt()
-        { }
+        public void SideLengt()
+        {
+            
+            length = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                length[i] = Math.Sqrt(Math.Pow(points[(i + 1) % n].x - points[i].x, 2) + Math.Pow(points[(i + 1) % n].y - points[i].y,2));
+                Console.WriteLine((i+1) + " side length = " + length[i]);
+            }
+        }
+
+        public double Perimeter()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                perimeter += length[i];
+            }
+            return perimeter;
+        }
+
+        public void Diagonals()
+        {
+            diagonal = new double[2];
+            Console.WriteLine("Diagonal length: " );
+            for (int i = 0; i < 2; i++)
+            {
+                diagonal[i] = Math.Sqrt(Math.Pow(points[(i + 2)].x - points[i].x, 2) + Math.Pow(points[(i + 2)].y - points[i].y, 2));
+                Console.WriteLine((i+1) + ": " + diagonal[i]);
+            }
+        }
+
+        public double Square()
+        {
+            double p = perimeter / 2;
+            square = Math.Sqrt((p - length[0]) * (p - length[1]) * (p - length[2]) * (p - length[3]));
+            return square;
+        }
     }
 }
